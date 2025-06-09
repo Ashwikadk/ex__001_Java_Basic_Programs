05a0f00e86199baa7d2c3326da8ff805833b9be6
Ashwikadk <ashwikadk017@gmail.com>
2025-06-09 21:18:22 +0530

    9th june

---
+++ src/JavaPracicePackage/Lab010_Identifiers.java
@@ -0,0 +1,10 @@
+package JavaPracicePackage;
+
+public class Lab010_Identifiers
+{
+    //identifier can start with $ _ not other spl character
+
+    public static void main(String[] args) {
+        //main method
+    }
+}
